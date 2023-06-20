8887d7e920b28d6c81de5ec6bcf447032cc09ad1
afzal-swe <afzalbhola07@gmail.com>
2023-06-20 06:50:57 +0600

    Customaization Frontend Design !

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ use App\Http\Controllers\Admin\GalleryController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.index');
 });
 
 Route::get('/dashboard', function () {
